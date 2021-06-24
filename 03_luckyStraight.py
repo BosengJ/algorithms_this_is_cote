5bb4b3f2f8945bbc641c590d7e5328015c94f553
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-06-24 14:29:08 +0900

    Update 03_luckyStraight.py

--- 03_luckyStraight.py
+++ 03_luckyStraight.py
@@ -3,12 +3,39 @@ def solution(N):
     idx = len(str_N)/2
     back = 0
     forth = 0
-    for i in len(str_N):
+    for i in range(len(str_N)):
         if i < idx:
-            back += str_N[i]
+            back += int(str_N[i])
         else:
-            forth += str_N[i]
-    if back = forth:
+            forth += int(str_N[i])
+    if back == forth:
         return "LUCKY"
     else:
         return "READY"
+
+N = 1000
+a = solution(N)
+print(a)
+
+'''test case
+"123402"
+LUCKY
+
+"7755"
+READY
+
+"10"
+READY
+
+"11"
+LUCKY
+
+"99999999"
+LUCKY
+
+"99999998"
+READY
+
+"1000"
+READY
+'''
