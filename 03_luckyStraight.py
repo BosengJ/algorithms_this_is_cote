170ae42561afe15aa2aca19473833bcb295558d8
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-06-24 15:00:22 +0900

    commenting

--- 03_luckyStraight.py
+++ 03_luckyStraight.py
@@ -1,8 +1,8 @@
 def solution(N):
     str_N = str(N)
     idx = len(str_N)/2
-    back = 0
-    forth = 0
+    back = 0                        # 둘로 나눠진 부분의 앞
+    forth = 0                       # 둘로 나눠진 부분의 뒤
     for i in range(len(str_N)):
         if i < idx:
             back += int(str_N[i])
--- 04_stringSort.py
+++ 04_stringSort.py
@@ -1,4 +1,4 @@
-def bubbleSort(li):
+def bubbleSort(li):                            # 오름차순 함수
     for i in range(len(li)-1):
         for j in range(len(li)-i-1):
             if li[j] > li[j+1]:
@@ -9,12 +9,12 @@ def solution(S):
     alphabet = []
     number = []
     for c in S:
-        if (ord(c) >= 65) and (ord(c) <= 90): # 대문자 아스키코드
+        if (ord(c) >= 65) and (ord(c) <= 90): # 대문자를 아스키코드로 찾아낸다
             alphabet.append(c)
         else:
             number.append(c)
-    sort_alphabet_li = bubbleSort(alphabet)
-    sort_alphabet = ''.join(sort_alphabet_li)
+    sort_alphabet_li = bubbleSort(alphabet)   # 알파벳을 오름차순 함수에 넣어 정렬한다
+    sort_alphabet = ''.join(sort_alphabet_li) # 리스트를 문자열로 바꿔준다
     sum_number = 0
     for n in number:
         sum_number += int(n)
