922806d7152ea649f7d1a9b053cd1ea9e687918e
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-06-25 21:06:00 +0900

    Update 05_game.py

--- 05_game.py
+++ 05_game.py
@@ -17,9 +17,9 @@ def searchLand(map,location):
 # 현재 바라보는 방향을 기준으로 왼쪽에 무슨 값이 있는지 알아내는 함수
 def searchLeftValue(location,d,map):
     i,j = location
-    if (j-1 >= 0) and (d == 0):             # 북
+    if (j-1 >= 0) and (d == 0):              # 북
         left_val = map[i][j-1]
-    elif (i-1 >= 0) and (d == 1):           # 동
+    elif (i-1 >= 0) and (d == 1):            # 동
         left_val = map[i-1][j]
     elif (j+1 < len(map[0])) and (d == 2):   # 남
         left_val = map[i][j+1]
@@ -33,12 +33,27 @@ def solution(map,location):
     current_location = location[:2]
     d = location[-1]
     y,x = current_location
-    map[y][x] = 2               # 방문한 곳은 2로 표시해준다. 시작 위치도 방문했기 때문에 2로 바꿔줬다.
+    map[y][x] = 2        # 방문한 곳은 2로 표시해준다. 시작 위치도 방문했기 때문에 2로 바꿔줬다.
     
     # 네 방향 하나라도 육지가 있을 경우
     if searchLand(map,current_location) == True:
         left_val = searchLeftValue(current_location,d,map)
+        print(map)
+        print(current_location)
         print(left_val)
+        if left_val == '0': # 육지가 존재한다면
+            d -= 1          # 방향을 왼쪽으로 이동한 다음
+            if d < 0:
+                d = 3       # 단, 방향이 북에서 서로 바뀔 때는 d 값이 -1 되기 때문에 3으로 다시 맞춰준다.
+            # 이 부분이 반복되므로 위의 함수 모두 다 기능 하나로 정리해준다.
+            if d == 0:
+            elif d == 1:
+            elif d == 2:
+            elif d == 3:
+
+            print('aa')
+        else:
+            print('bb')
     
     # 네 방향 모두 이미 가본 칸이거나 바다로 되어있는 경우
     else:
