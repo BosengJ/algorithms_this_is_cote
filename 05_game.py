3bc6a825bb389484efbd9fd2bb5fa5fd23b1d642
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-06-25 19:46:56 +0900

    Update 05_game.py

--- 05_game.py
+++ 05_game.py
@@ -1,9 +1,11 @@
 def solution(map,location):
 
 
-    
+
     answer = []
     return answer
 
 map = [[1,1,1,1],[1,0,0,1],[1,1,0,1],[1,1,1,1]]
 location = [1,1,0]
+a = solution(map,location)
+print(a)
