db7f4a0a055fa99dcfa964b66c3823140a86aee2
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-06-26 12:50:24 +0900

    Update 05_game.py

--- 05_game.py
+++ 05_game.py
@@ -65,6 +65,13 @@ def solution(map,location):
     y,x = current_location
     map[y][x] = 2            # 방문한 곳은 2로 표시해준다 (시작 위치도 방문했기 때문에 2로 바꿔줬다)
     
+    # 초기값 출력
+    print('start!')
+    print('map:',map)
+    print('current_location:',current_location)
+    print('d:',d)
+
+
     # while 처리 해준다 (조건이 끝날 때 까지 무한 반복 해야한다)
     # 현재 위치를 기준으로 네 방향의 값을 확인한다
     n_val = find_northVal(map,current_location)
@@ -77,10 +84,7 @@ def solution(map,location):
 
     # 네 방향 하나라도 육지가 있을 경우
     if (n_val == 0) or (e_val == 0) or (s_val == 0) or (w_val == 0): 
-        print('map:',map)
-        print('current_location:',current_location)
-        print('d:',d)
-        print('there is land')
+        print('one of 4 direction, there is land')
 
         # 현재 바라보는 방향을 기준으로 왼쪽값(left_val)을 확인해본다
         if d == 0:               # 북쪽을 바라보고 있다면 왼쪽인 서쪽 값을 구한다
@@ -97,6 +101,7 @@ def solution(map,location):
 
         # 1번조건) 왼쪽값(left_val)이 육지이면서 아직 가보지 않은 경우
         if left_val == 0:
+            print('left! there is land')
             # 바라보는 방향을 왼쪽으로 이동시켜 준 다음,
             d -= 1
             if d < 0:
@@ -110,11 +115,16 @@ def solution(map,location):
                 current_location = find_southIdx(current_location)
             if d == 3:
                 current_location = find_westIdx(current_location)
+            print('d:',d)
+            print('current_location:',current_location)
+
+            # 이동한 곳의 값을 2로 바꿔준다.
+            y,x = current_location
+            map[y][x] = 2  
+            print('map:',map)
+
             
-            
-            print('left! there is land')
-            
-        # 2번조건) 왼쪽값(left_val)이 육지가 아닌 경우
+        # 2번조건) 왼쪽값(left_val)이 육지가 아닌 경우, 바라보는 방향을 왼쪽으로 회전시켜준다
         else:
             d -= 1
             if d < 0:
@@ -124,7 +134,7 @@ def solution(map,location):
 
     # 네 방향 모두 이미 가본 칸이거나 바다로 되어있는 경우
     else:
-        print('no land')
+        print('4 direction, no land')
 
     return []
 
