1e6564dd17638d1fcaa76c97fcb0bab74319c035
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-06-26 17:21:07 +0900

    Update 05_game.py

--- 05_game.py
+++ 05_game.py
@@ -71,70 +71,94 @@ def solution(map,location):
     print('current_location:',current_location)
     print('d:',d)
 
+    while True:
+        # 현재 위치를 기준으로 네 방향의 값을 확인한다
+        n_val = find_northVal(map,current_location)
+        e_val = find_eastVal(map,current_location)
+        s_val = find_southVal(map,current_location)
+        w_val = find_westVal(map,current_location)
+        
+        '''현재 위치를 기준으로 네 방향의 값을 확인하여, 
 
-    # while 처리 해준다 (조건이 끝날 때 까지 무한 반복 해야한다)
-    # 현재 위치를 기준으로 네 방향의 값을 확인한다
-    n_val = find_northVal(map,current_location)
-    e_val = find_eastVal(map,current_location)
-    s_val = find_southVal(map,current_location)
-    w_val = find_westVal(map,current_location)
-    
-    '''현재 위치를 기준으로 네 방향의 값을 확인하여, 
-    1) 한 방향이라도 가보지 않은 육지가 있는 경우와 2) 그렇지 않은 경우로 나누어 진행한다.'''
+        1) 한 방향이라도 가보지 않은 육지가 있는 경우,
+            왼쪽값을 확인해서,
+            a) 왼쪽값이 육지이면서 아직 가보지 않은 경우
+            b) 왼쪽값이 육지가 아닌 경우로 나누어 진행한다
 
-    # 네 방향 하나라도 육지가 있을 경우
-    if (n_val == 0) or (e_val == 0) or (s_val == 0) or (w_val == 0): 
-        print('one of 4 direction, there is land')
+        2) 네 방향 모두 이미 가본 칸이거나 바다로 되어 있는 경우'''
 
-        # 현재 바라보는 방향을 기준으로 왼쪽값(left_val)을 확인해본다
-        if d == 0:               # 북쪽을 바라보고 있다면 왼쪽인 서쪽 값을 구한다
-            left_val = find_westVal(map,current_location)
-        if d == 1:               # 동쪽을 바라보고 있다면 왼쪽인 북쪽 값을 구한다
-            left_val = find_eastVal(map,current_location)
-        if d == 2:               # 남쪽을 바라보고 있다면 왼쪽인 동쪽 값을 구한다
-            left_val = find_eastVal(map,current_location)
-        if d == 3:               # 서쪽을 바라보고 있다면 왼쪽인 남쪽 값을 구한다
-            left_val = find_southVal(map,current_location)
-        print('left_val:',left_val)
+        # 1) 네 방향 하나라도 육지가 있을 경우
+        if (n_val == 0) or (e_val == 0) or (s_val == 0) or (w_val == 0): 
+            print('one of 4 direction, there is land')
 
-        # 확인한 왼쪽값(left_val)을 가지고 조건을 판별한다 (2가지 조건으로 다시 나눈다)
+            # 현재 바라보는 방향을 기준으로 왼쪽값(left_val)을 확인해본다
+            if d == 0:               # 북쪽을 바라보고 있다면 왼쪽인 서쪽 값을 구한다
+                left_val = find_westVal(map,current_location)
+            if d == 1:               # 동쪽을 바라보고 있다면 왼쪽인 북쪽 값을 구한다
+                left_val = find_eastVal(map,current_location)
+            if d == 2:               # 남쪽을 바라보고 있다면 왼쪽인 동쪽 값을 구한다
+                left_val = find_eastVal(map,current_location)
+            if d == 3:               # 서쪽을 바라보고 있다면 왼쪽인 남쪽 값을 구한다
+                left_val = find_southVal(map,current_location)
+            print('left_val:',left_val)
 
-        # 1번조건) 왼쪽값(left_val)이 육지이면서 아직 가보지 않은 경우
-        if left_val == 0:
-            print('left! there is land')
-            # 바라보는 방향을 왼쪽으로 이동시켜 준 다음,
-            d -= 1
-            if d < 0:
-                d = 3
-            # 바뀐 바라보는 방향으로 현재 위치를 한 칸 이동시켜준다
+            '''확인한 왼쪽값을 가지고
+            a) 왼쪽값이 육지이면서 아직 가보지 않은 경우
+            b) 왼쪽값이 육지가 아닌 경우로 나누어 진행한다
+            '''
+
+            # a) 왼쪽값(left_val)이 육지이면서 아직 가보지 않은 경우
+            if left_val == 0:
+                print('left! there is land')
+                # 바라보는 방향을 왼쪽으로 이동시켜 준 다음,
+                d -= 1
+                if d < 0:
+                    d = 3
+                # 바뀐 바라보는 방향으로 현재 위치를 한 칸 이동시켜준다
+                if d == 0:
+                    current_location = find_northIdx(current_location)
+                if d == 1:
+                    current_location = find_eastIdx(current_location)
+                if d == 2:
+                    current_location = find_southIdx(current_location)
+                if d == 3:
+                    current_location = find_westIdx(current_location)
+                print('d:',d)
+                print('current_location:',current_location)
+
+                # 이동한 곳의 값을 2로 바꿔준다.
+                y,x = current_location
+                map[y][x] = 2  
+                print('map:',map)
+
+            # b) 왼쪽값(left_val)이 육지가 아닌 경우, 바라보는 방향을 왼쪽으로 회전시켜준다
+            else:
+                d -= 1
+                if d < 0:
+                    d = 3
+                print('left! no land')
+                print('d:',d)
+
+        # 2) 네 방향 모두 이미 가본 칸이거나 바다로 되어있는 경우, 바라보는 방향을 유지한 채 뒤로 한 칸 이동한다
+        else:
+            print('4 direction, no land')
             if d == 0:
-                current_location = find_northIdx(current_location)
-            if d == 1:
-                current_location = find_eastIdx(current_location)
-            if d == 2:
                 current_location = find_southIdx(current_location)
-            if d == 3:
+            if d == 1:
                 current_location = find_westIdx(current_location)
-            print('d:',d)
-            print('current_location:',current_location)
+            if d == 2:
+                current_location = find_northIdx(current_location)
+            if d == 3:
+                current_location = find_eastIdx(current_location)
 
             # 이동한 곳의 값을 2로 바꿔준다.
             y,x = current_location
-            map[y][x] = 2  
+            current_val = map[y][x]
+            if current_val == 0:
+                map[y][x] = 2  
+            if current_val == 1:
+                break
             print('map:',map)
-
-            
-        # 2번조건) 왼쪽값(left_val)이 육지가 아닌 경우, 바라보는 방향을 왼쪽으로 회전시켜준다
-        else:
-            d -= 1
-            if d < 0:
-                d = 3
-            print('left! no land')
-            print('d:',d)
-
-    # 네 방향 모두 이미 가본 칸이거나 바다로 되어있는 경우
-    else:
-        print('4 direction, no land')
 
     return []
 
