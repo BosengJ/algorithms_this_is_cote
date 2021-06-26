68137f3fdad910d53d75b5c38bf9e9fc1b392647
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-06-26 12:33:25 +0900

    Update 05_game.py

--- 05_game.py
+++ 05_game.py
@@ -1,4 +1,4 @@
-# 현재 위치를 기준으로 사방에 어떤 값이 있는지 판별하는 함수 4개
+# 현재 위치를 기준으로 사방에 어떤 값이 있는지 확인하는 함수 4개
 
 def find_northVal(map,current_location):    # 북쪽의 값
     i,j = current_location
@@ -14,8 +14,8 @@ def find_eastVal(map,current_location):     # 동쪽의 값
         e_val = map[i][j+1]     
     else:
         e_val = 'none'
-    return e_val
-    
+    return e_val 
+
 def find_southVal(map,current_location):    # 남쪽의 값
     i,j = current_location
     if i+1 < len(map):
@@ -32,12 +32,38 @@ def find_westVal(map,current_location):     # 서쪽의 값
         w_val = 'none'
     return w_val
 
+# 바라보는 방향을 기준으로, 한칸 이동했을 때의 위치를 확인하는 함수 4개
+
+def find_northIdx(current_location):        # 북쪽으로 이동했을 때의 인덱스
+    i,j = current_location
+    i -= 1
+    return [i,j]
+
+def find_eastIdx(current_location):         # 동쪽으로 이동했을 때의 인덱스
+    i,j = current_location
+    j += 1
+    return [i,j]
+
+def find_southIdx(current_location):        # 남쪽으로 이동했을 때의 인덱스
+    i,j = current_location
+    i += 1
+    return [i,j]
+
+def find_westIdx(current_location):         # 서쪽으로 이동했을 때의 인덱스
+    i,j = current_location
+    j -= 1
+    return [i,j]
+
+
+
+
+
+
 def solution(map,location):
     current_location = location[:2]
     d = location[-1]
     y,x = current_location
     map[y][x] = 2                           # 방문한 곳은 2로 표시해준다 (시작 위치도 방문했기 때문에 2로 바꿔줬다)
-    
     
     # while 처리 해준다 (조건이 끝날 때 까지 무한 반복 해야한다)
     # 현재 위치를 기준으로 네 방향의 값을 확인한다
@@ -66,6 +92,13 @@ def solution(map,location):
 
         # 왼쪽 값이 육지이면서 아직 가보지 않은 경우
         if left_val == 0:
+            # 바라보는 방향을 왼쪽으로 이동시켜 준 다음,
+            d -= 1
+            if d < 0:
+                d = 3
+            # 바뀐 바라보는 방향으로 현재 위치를 한 칸 이동시켜준다
+            
+            
             print('left! there is land')
             
         # 왼쪽 값이 육지가 아닌 경우
