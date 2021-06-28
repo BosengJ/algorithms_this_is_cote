0e8e73491762b0a995ad708336f8bc906e82e52e
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-06-28 19:12:17 +0900

    add new code

--- 01_knight.py
+++ 01_knight.py
@@ -1,38 +1,61 @@
-def leftRight(column,cnt):
-    if column-1 > 0:
-        cnt += 1
-    if column+1 < 9:
-        cnt += 1
-    return cnt
 
-def upDown(row,cnt):
-    if row-1 > 0:
-        cnt += 1
-    if row+1 < 9:
-        cnt += 1
-    return cnt
 
 def solution(data):
     data_li = list(data)
+
+    # 행과 열 구하기
     row = int(data_li[1])
-    
-    # column 구하기
-    column = int(ord(data_li[0])- ord('a'))
-    # column = int(chr(askii_num))
+    column = int(ord(data_li[0])- ord('a'))+1
 
-    # 나이트가 위 또는 아래로 2칸 움직일 때
+    # 8가지 경우의 수
+    steps = [[-2,-1],[-1,-2],[1,-2],[2,-1],[2,1],[1,2],[-1,2],[-2,1]]
+
+    # 8가지 경우의 수로 이동이 가능한지 확인하고 카운트한다
     cnt = 0
-    if (row-2 > 0):
-        cnt = leftRight(column,cnt) # 왼쪽, 오른쪽으로 1칸 움직일 때
-    if (row+2 < 9):
-        cnt = leftRight(column,cnt)
-
-    # 나이트가 왼쪽 또는 오른쪽으로 2칸 움직일 때
-    if (column-2 > 0):
-        cnt = upDown(row,cnt) # 위, 아래로 1칸 움직일 때
-    if (column+2 < 9):
-        cnt = upDown(row,cnt)
+    for step in steps:
+        next_row = row + step[0]
+        next_column = column + step[1]
+        if 1 <= next_row and next_row <= 8 and 1 <= next_column and next_column <= 8:
+            cnt += 1
     return cnt
+
+
+
+# def leftRight(column,cnt):
+#     if column-1 > 0:
+#         cnt += 1
+#     if column+1 < 9:
+#         cnt += 1
+#     return cnt
+
+# def upDown(row,cnt):
+#     if row-1 > 0:
+#         cnt += 1
+#     if row+1 < 9:
+#         cnt += 1
+#     return cnt
+
+# def solution(data):
+#     data_li = list(data)
+#     row = int(data_li[1])
+    
+#     # column 구하기
+#     column = int(ord(data_li[0])- ord('a'))
+#     # column = int(chr(askii_num))
+
+#     # 나이트가 위 또는 아래로 2칸 움직일 때
+#     cnt = 0
+#     if (row-2 > 0):
+#         cnt = leftRight(column,cnt) # 왼쪽, 오른쪽으로 1칸 움직일 때
+#     if (row+2 < 9):
+#         cnt = leftRight(column,cnt)
+
+#     # 나이트가 왼쪽 또는 오른쪽으로 2칸 움직일 때
+#     if (column-2 > 0):
+#         cnt = upDown(row,cnt) # 위, 아래로 1칸 움직일 때
+#     if (column+2 < 9):
+#         cnt = upDown(row,cnt)
+#     return cnt
 
 data = 'c8'
 answer = solution(data)
