bd9a9fcf1bc779397828a79c9bb24e28c2d2ac93
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-06-29 17:31:19 +0900

    Update 07_lock_key.py

--- 07_lock_key.py
+++ 07_lock_key.py
@@ -1,3 +1,16 @@
+def calDistance(li):
+    distance_i = 0
+    distance_j = 0
+    for i,j in li:
+        distance_i -= i
+        distance_j -= j
+        if distance_i < 0:
+            distance_i *= -1
+        if distance_j < 0:
+            distance_j *= -1
+    dx = [distance_i,distance_j]
+    return dx
+
 def solution(key, lock):
     idx_lock_list = []
     cnt_zero = 0
@@ -7,17 +20,36 @@ def solution(key, lock):
                 cnt_zero += 1
                 idx_zero = [i,j]
                 idx_lock_list.append(idx_zero)
-    distance_i = 0
-    distance_j = 0
-    for i,j in idx_lock_list:
-        distance_i -= i
-        distance_j -= j
-        if distance_i < 0:
-            distance_i *= -1
-        if distance_j < 0:
-            distance_j *= -1
-    distance = [distance_i,distance_j]
-    print(distance)
+    distance_lock = calDistance(idx_lock_list)
+    print(idx_lock_list)
+    print(distance_lock)
+    print(cnt_zero)
+    
+    # idx_key_list = []
+    # cnt_one = 0
+    # for i in range(len(key)):
+    #     for j in range(len(key)):
+    #         if lock[i][j] == 1:
+    #             cnt_one += 1
+    #             idx_one = [i,j]
+    #             idx_key_list.append(idx_one)
+    # if cnt_one == cnt_zero:
+    #     distance_i = 0
+    #     distance_j = 0
+    #     for i,j in idx_key_list:
+    #         distance_i -= i
+    #         distance_j -= j
+    #         if distance_i < 0:
+    #             distance_i *= -1
+    #         if distance_j < 0:
+    #             distance_j *= -1
+    #     distance = [distance_i,distance_j]
+    # if distance == distance:
+    #     return true
+        
+        
+    
+        
     
     answer = True
     return answer
