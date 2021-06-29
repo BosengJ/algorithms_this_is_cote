8e8f2babf8e75426365fc6b2655ed37d4424e8ae
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-06-29 20:41:51 +0900

    Update 07_lock_key.py

--- 07_lock_key.py
+++ 07_lock_key.py
@@ -1,15 +1,22 @@
+# def checkLock(li):
+#     for i in range(len(li))
+
 def solution(key, lock):
     # 확장된 Lock array
     extended_lock = []
     for i in range(len(lock)*3):
         extended_lock.append([0]*len(lock)*3)
+        
     # 확장된 Lock array 에 기존의 lock array를 가운데에 넣어준다
-    for i in range(len(extended_lock)):
-        for j in range(len(extended_lock)):
-            if i >= len(lock) and i < len(lock)*2 and j >= len(lock) and j < len(lock)*2:
-                extended_lock[i][j] += lock[i-len(lock)][j-len(lock)]
+    for i in range(len(lock),len(lock)*2):
+        for j in range(len(lock),len(lock)*2):
+            extended_lock[i][j] += lock[i-len(lock)][j-len(lock)]
     print(extended_lock)
-                
+    
+    # 0도 회전한 key를 확장된 lock에 더해주기
+    # for i in range(len(extended_lock)):
+    #     for j in range(len(extended_lock)):
+        
     
     answer = True
     return answer
