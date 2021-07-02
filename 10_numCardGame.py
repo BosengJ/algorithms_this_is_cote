b5231b63f9ae0042ac1846a81a79510d8f83e244
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-07-02 15:04:10 +0900

    Create 10_numCardGame.py

---
+++ 10_numCardGame.py
@@ -0,0 +1,20 @@
+n,m = map(int,input("n,m을 공백을 구분으로 입력하세요:").split())
+card_arr = []
+for i in range(n):
+    card_arr.append(list(map(int,input("n,m을 공백을 구분으로 입력하세요:").split())))
+print(card_arr)
+
+min_li = []
+for i in range(n):
+    min_num = card_arr[i][0]
+    for j in range(m):
+        if card_arr[i][j] < min_num:
+            min_num = card_arr[i][j]
+    min_li.append(min_num)
+print(min_li)
+
+max_num = 0
+for num in min_li:
+    if num > max_num:
+        max_num = num
+print(max_num)
