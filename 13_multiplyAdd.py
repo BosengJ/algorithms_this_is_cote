0be3536fe1d8d412b8eb685dd055b8f4804b6d84
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-07-05 09:26:17 +0900

    Create 13_multiplyAdd.py

---
+++ 13_multiplyAdd.py
@@ -0,0 +1,11 @@
+s_list = list(map(int,input("0부터 9로만 이루어진 문자열을 입력하세요")))
+
+answer = 0
+for n in s_list:
+    if answer == 0:
+        answer += n
+    elif n == 0:
+        answer += n
+    else:
+        answer *= n
+print(answer)
