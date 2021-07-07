1fc74c8e0e756832a394d57e85270e2767ff26e3
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-07-07 14:15:47 +0900

    Update 13_multiplyAdd.py

--- 13_multiplyAdd.py
+++ 13_multiplyAdd.py
@@ -2,10 +2,24 @@ s_list = list(map(int,input("0부터 9로만 이루어진 문자열을 입력하
 
 answer = 0
 for n in s_list:
-    if answer == 0:
+    if answer <= 1:
         answer += n
     elif n <= 1:
         answer += n
     else:
         answer *= n
 print(answer)
+
+# 곱하기 혹은 더하기
+# 테스트케이스
+# 02984
+# 576
+
+# 20984
+# 576
+
+# 12345
+# 180
+
+# 20914
+# 76
