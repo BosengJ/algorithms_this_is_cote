615702a87b24667d937e4478de02d90b15d9d4f8
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-07-14 11:01:18 +0900

    Create 22_factorial.py

---
+++ 22_factorial.py
@@ -0,0 +1,17 @@
+# p132 2가지 방식으로 구현한 팩토리얼 예제
+
+# 방법1: 반복적으로 구현한 n!
+def factorial_iterative(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+# 방법2: 재귀적으로 구현한 n!
+def factorial_recursive(n):
+    if n <= 1:
+        return 1
+    return n * factorial_recursive(n-1)
+
+print('반복적으로 구현:', factorial_iterative(5))
+print('재귀적으로 구현:', factorial_recursive(5))
