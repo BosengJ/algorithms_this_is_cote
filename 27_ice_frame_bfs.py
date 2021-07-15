e422e47159421a71eee5d0d975c776efe20b1842
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-07-15 16:28:19 +0900

    Update 27_ice_frame_bfs.py

--- 27_ice_frame_bfs.py
+++ 27_ice_frame_bfs.py
@@ -13,13 +13,23 @@ ice_frame = [
     [0, 0, 0, 0, 0]
     ]
 
-def check0_turn1(li):
+def check0_turn1(li,x,y,m,n):
+    queue = deque([x,y])
+    li[x][y] = 1
+    while queue:
+        p,q = queue.popleft()
+        if (q+1 < m) and (li[p][q+1] == 0): # 오른쪽 확인
+            queue.append([p,q+1])
+            li[p][q+1] = 1
+        if (p+1 < n) and (li[p+1][q] == 0): # 왼쪽 확인
+            queue.append([p+1,q])
+            li[p+1][q] = 1
     return li
 
 icecream = 0
 for i in range(n):
     for j in range(m):
         if ice_frame[i][j] == 0:
-            ice_frame = check0_turn1()
+            ice_frame = check0_turn1(ice_frame,i,j,m,n)
             icecream += 1
 print(icecream)
