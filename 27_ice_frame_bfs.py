e55b33d3767fc7550a5be3b789a63166edf0b698
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-07-15 15:26:16 +0900

    Update 27_ice_frame_bfs.py

--- 27_ice_frame_bfs.py
+++ 27_ice_frame_bfs.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # n,m = map(int,input("n,m을 공백을 구분으로 입력하세요:").split())
 # ice_frame = []
 # for i in range(n):
@@ -10,3 +12,14 @@ ice_frame = [
     [1, 1, 1, 1, 1], 
     [0, 0, 0, 0, 0]
     ]
+
+def check0_turn1(li):
+    return li
+
+icecream = 0
+for i in range(n):
+    for j in range(m):
+        if ice_frame[i][j] == 0:
+            ice_frame = check0_turn1()
+            icecream += 1
+print(icecream)
