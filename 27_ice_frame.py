ef2a7583dd87f390c33ee1d9e6677d05e0e2ae9c
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-07-15 13:05:16 +0900

    Create 27_ice_frame.py

---
+++ 27_ice_frame.py
@@ -0,0 +1,14 @@
+# n,m = map(int,input("n,m을 공백을 구분으로 입력하세요:").split())
+# ice_frame = []
+# for i in range(n):
+#     ice_frame.append(list(str(input("얼음 틀 형태를 입력하세요:"))))
+# print(n,m)
+# print(ice_frame)
+
+n,m = 4, 5
+ice_frame = [
+    ['0', '0', '1', '1', '0'], 
+    ['0', '0', '0', '1', '1'], 
+    ['1', '1', '1', '1', '1'], 
+    ['0', '0', '0', '0', '0']
+    ]
