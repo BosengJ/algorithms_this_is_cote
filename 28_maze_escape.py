44ec36f1e5fc7b22be2b35fa72a83d84ea348c93
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-07-18 14:42:02 +0900

    Update 28_maze_escape.py

--- 28_maze_escape.py
+++ 28_maze_escape.py
@@ -5,7 +5,7 @@ from collections import deque
 # for i in range(n):
 #     maze.append(list(map(int,str(input("미로의 정보를 입력하세요:")))))
 
-
+print('============================================')
 n,m = 5,6
 maze = [
     [1, 0, 1, 0, 1, 0], 
@@ -15,20 +15,21 @@ maze = [
     [1, 1, 1, 1, 1, 1]
     ]
 
-
-
 # 초기값 설정
 queue = deque([[0,0]])
-visited = 1
 maze[0][0] = 0
+visited = 0
 
 for i in range(n):
     for j in range(m):
         while queue:
-            print('queue:',queue, 'v:',visited)
-            print(maze)
-            print()
+            print('queue:',queue)
             p,q = queue.popleft()
+            visited += 1
+
+            print(maze)
+            print('v:',visited)
+            print()
 
             if (p == n-1) and (q == m-1):   # 출구 도착시 종료
                 visited -= len(queue)
@@ -36,22 +37,18 @@ for i in range(n):
 
             if (p-1 >= 0) and (maze[p-1][q] == 1):  # 위쪽 확인
                 queue.append([p-1,q])
-                visited += 1
                 maze[p-1][q] = 0
 
             if (q-1 >= 0) and (maze[p][q-1] == 1):  # 왼쪽 확인
                 queue.append([p,q-1])
-                visited += 1
                 maze[p][q-1] = 0
 
             if (q+1 < m) and (maze[p][q+1] == 1):  # 오른쪽 확인
                 queue.append([p,q+1])
-                visited += 1
                 maze[p][q+1] = 0
 
             if (p+1 < n) and (maze[p+1][q] == 1):  # 아래쪽 확인
                 queue.append([p+1,q])
-                visited += 1
                 maze[p+1][q] = 0
 
             else:
