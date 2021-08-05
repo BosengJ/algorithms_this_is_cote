5da0d04c6036147317bd1a7083723892ec382ed0
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-08-05 17:09:17 +0900

    Delete 29_find_city.py

--- 29_find_city.py
+++
@@ -1,9 +0,0 @@
-# p339
-
-n,m,k,x = map(int, input("n,m,k,x을 공백을 구분으로 입력하세요:").split())
-city = []
-for i in range(m):
-    city.append(list(map(int,(input("도시의 정보를 입력하세요:").split()))))
-
-
-print(city)
