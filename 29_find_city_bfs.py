a88d0adbf5944fb86f362b2b01993d80b8e8d56e
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-08-05 20:31:58 +0900

    Update 29_find_city_bfs.py

--- 29_find_city_bfs.py
+++ 29_find_city_bfs.py
@@ -12,6 +12,7 @@ import sys
 n,m,k,x = 4,4,2,1
 city = [[1, 2], [1, 3], [2, 3], [2, 4]]
 
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현
 city_node = []
 for c in range(m+1):
     node = []
@@ -21,3 +22,23 @@ for c in range(m+1):
                 node.append(city[i][j+1])
     city_node.append(node)
 print(city_node)
+
+# 각 도시별(key) 최단 거리(value)를 나타내는 사전 만들기
+city_dict = {}
+c = 1
+for i in range(n):
+    city_dict[c] = []
+    c += 1
+
+# 초기값으로 출발 도시를 넣어준다
+queue = deque([x])
+distance = 0
+
+while queue:
+    c = queue.popleft()
+    city_dict[c] = distance
+    for i in city_node[c]:
+        queue.append(i)
+    distance += 1
+
+print(city_dict)
