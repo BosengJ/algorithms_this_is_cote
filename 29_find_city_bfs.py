d46047d86f282ec5b9ba56b4480f5fce91398ce5
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-08-05 17:11:58 +0900

    Update 29_find_city_bfs.py

--- 29_find_city_bfs.py
+++ 29_find_city_bfs.py
@@ -12,22 +12,3 @@ import sys
 n,m,k,x = 4,4,2,1
 city = [[1, 2], [1, 3], [2, 3], [2, 4]]
 
-# 각 도시별(key) 최단 거리(value)를 나타내는 사전 만들기
-city_dict = {}
-c = 1
-for i in range(n):
-    city_dict[c] = []
-    c += 1
-
-# 출발도시 x 최단거리 값 처리
-city_dict[x].append(0)
-
-# 출발도시 x 를 기준으로 최단거리 구하기
-distance = 1
-for i in range(m):
-    for j in range(2):
-        if (j == 0) and (city[i][j] == x):
-            arrival = city[i][j+1]
-            city_dict[arrival].append(distance)
-
-print(city_dict)
