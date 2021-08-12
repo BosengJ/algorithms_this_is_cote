b933041cbcb881e17fc8eeea3da00f140c57e390
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-08-12 10:02:38 +0900

    Update 12_문자열재정렬.py

--- 복습/12_문자열재정렬.py
+++ 복습/12_문자열재정렬.py
@@ -1,6 +1,7 @@
 # p322 문자열 재정렬
 
-s = 'K1KA5CB7'
+s = input("문자열 S를 입력하세요:")
+# s = 'K1KA5CB7'
 
 def bubbleSort(li):
     for i in range(len(li)-1):
@@ -9,3 +10,14 @@ def bubbleSort(li):
                 li[j], li[j+1] = li[j+1], li[j]
     return li
 
+sorted_s = bubbleSort(list(s))
+
+num = 0
+answer = ''
+for c in sorted_s:
+    if (48 <= ord(c)) and (ord(c) <= 57):
+        num += int(c)
+    else:
+        answer += c
+answer += str(num)
+print(answer)
