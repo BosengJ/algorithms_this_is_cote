7b8fe33b037ebfc2448f7d3943037f1bfd632577
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-08-13 10:51:57 +0900

    Update 15_음료수얼려먹기.py

--- 복습/15_음료수얼려먹기.py
+++ 복습/15_음료수얼려먹기.py
@@ -1,8 +1,9 @@
 # p149 음료수 얼려 먹기
 
-n,m = map(int,input("공백을 기준으로 n,m을 입력하세요:").split())
-icebox = []
-for i in range(n):
-    icebox.append(list(map(int,input("얼음 틀의 형태를 입력하세요:"))))
+# n,m = map(int,input("공백을 기준으로 n,m을 입력하세요:").split())
+# icebox = []
+# for i in range(n):
+#     icebox.append(list(map(int,input("얼음 틀의 형태를 입력하세요:"))))
 
-print(icebox)
+icebox = [[0, 0, 1, 1, 0], [0, 0, 0, 1, 1], [1, 1, 1, 1, 1], [0, 0, 0, 0, 0]]
+
