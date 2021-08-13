129d3163ea16a85bb87704c2a98cc71093bfde27
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-08-13 15:42:35 +0900

    Create 17_특정거리의도시찾기.py

---
+++ 복습/17_특정거리의도시찾기.py
@@ -0,0 +1,17 @@
+# p339 특정 거리의 도시 찾기
+
+# n,m,k,x = map(int,input("공백을 기준으로 n,m,k,x를 입력하세요:").split())
+# information = []
+# for i in range(m):
+#     information.append(list(map(int,input("도시A에서 도시B로의 도로를 공백으로 구분하여 A,B를 입력하세요:").split())))
+
+information = [[1, 2], [1, 3], [2, 3], [2, 4]]
+
+
+
+# a = []
+# b = 1
+# if (len(a) == 0) or (b < a[0]):
+#     a.append(b)
+
+# print(a)
