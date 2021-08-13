464d60622b3ee66ba812c29569337a97175548af
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-08-13 14:39:09 +0900

    Update 16_미로탈출.py

--- 복습/16_미로탈출.py
+++ 복습/16_미로탈출.py
@@ -2,19 +2,19 @@
 
 from collections import deque
 
-# n,m = map(int,input("n,m을 공백으로 구분하여 입력하세요:").split())
-# maps = []
-# for i in range(n):
-#     maps.append(list(map(int,input("미로의 정보를 입력하세요:"))))
+n,m = map(int,input("n,m을 공백으로 구분하여 입력하세요:").split())
+maps = []
+for i in range(n):
+    maps.append(list(map(int,input("미로의 정보를 입력하세요:"))))
 
-n,m = 5,6
-maps = [
-    [1, 0, 1, 0, 1, 0], 
-    [1, 1, 1, 1, 1, 1], 
-    [0, 0, 0, 0, 0, 1], 
-    [1, 1, 1, 1, 1, 1], 
-    [1, 1, 1, 1, 1, 1]
-    ]
+# n,m = 5,6
+# maps = [
+#     [1, 0, 1, 0, 1, 0], 
+#     [1, 1, 1, 1, 1, 1], 
+#     [0, 0, 0, 0, 0, 1], 
+#     [1, 1, 1, 1, 1, 1], 
+#     [1, 1, 1, 1, 1, 1]
+#     ]
 
 def bfs(x,y,li,n,m):
     queue = deque([[x,y]])
