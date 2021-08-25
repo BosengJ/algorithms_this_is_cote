8d47d0fdbee1505fe15b8963a670022f7564daf9
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-08-25 12:02:44 +0900

    Update 30_연구소.py

---
+++ 30_연구소.py
@@ -0,0 +1,14 @@
+# 백준 https://www.acmicpc.net/problem/14502
+
+# n,m = map(int,input("n,m >"))
+# maps = []
+# for i in range(n):
+#     maps.append(list(map(int,input("maps >"))))
+
+n,m = 4,6
+maps = [
+    [0, 0, 0, 0, 0, 0], 
+    [1, 0, 0, 0, 0, 2], 
+    [1, 1, 1, 0, 0, 2], 
+    [0, 0, 0, 0, 0, 2]
+    ]
