5c247d6c7e32fa09ded04a25db6ac26051a5c901
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-08-25 12:06:14 +0900

    Update 30_연구소.py

--- 30_연구소.py
+++ 30_연구소.py
@@ -12,3 +12,14 @@ maps = [
     [1, 1, 1, 0, 0, 2], 
     [0, 0, 0, 0, 0, 2]
     ]
+
+cnt = 0
+for i in range(n):
+    for j in range(m):
+        if (cnt<3) and (maps[i][j] == 0):
+            maps[i][j] = 1
+            cnt += 1
+        if cnt == 3:
+            # 바이러스 -> 0 cnt -> max와 비교
+            maps[i][j] = 0
+            cnt -= 1
