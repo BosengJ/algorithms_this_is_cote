cf3f8f2f31f6b2972380a179769ab3a8f209bf84
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-08-31 11:17:50 +0900

    Update 31_경쟁적전염_bfs.py

--- 31_경쟁적전염_bfs.py
+++ 31_경쟁적전염_bfs.py
@@ -10,25 +10,25 @@ n,k = 3,3
 tube = [[1, 0, 2], [0, 0, 0], [3, 0, 0]]
 s,x,y = 1,2,2
 
-
+# 초기값 설정
 q = deque([])
 for virus in range(1,k+1):
     for i in range(n):
         for j in range(n):
             if tube[i][j] == virus:
-                q.append([virus,i,j])
+                q.append([0,virus,i,j])
 
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
 
 while q:
-    v, vx, vy = q.popleft()
+    t, v, vx, vy = q.popleft()   # t(경과시간), v(바이러스종류), vx(행 위치값), vy(열 위치값)
     for i in range(4):
         nx = vx + dx[i]
         ny = vy + dy[i]
-        if (nx >= 0) and (nx < n) and (ny >= 0) and (ny < n):
+        if (t < s) and (nx >= 0) and (nx < n) and (ny >= 0) and (ny < n):
             if tube[nx][ny] == 0:
-                q.append([v,nx,ny])
+                q.append([t+1, v, nx, ny])
                 tube[nx][ny] = v
 
 answer = tube[x-1][y-1]
