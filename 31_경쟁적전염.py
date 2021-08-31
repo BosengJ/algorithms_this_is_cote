f62bd062ccd39acfdeae0076182f089a5c525803
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-08-31 10:06:06 +0900

    Create 31_경쟁적전염.py

---
+++ 31_경쟁적전염.py
@@ -0,0 +1,9 @@
+# n,k = map(int,input().split())
+# tube = []
+# for i in range(n):
+#     tube.append(list(map(int,input().split())))
+# s,x,y = map(int,input().split())
+
+n,k = 3,3
+tube = [[1, 0, 2], [0, 0, 0], [3, 0, 0]]
+s,x,y = 2,3,2
