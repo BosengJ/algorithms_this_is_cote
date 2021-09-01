72500d160f87406267cbf6c399e9ac9af29520f0
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-09-01 13:57:36 +0900

    Update 31_경쟁적전염_bfs.py

--- 31_경쟁적전염_bfs.py
+++ 31_경쟁적전염_bfs.py
@@ -1,27 +1,50 @@
 from collections import deque
 import sys
 
-# n,k = map(int,sys.stdin.readline().split())
-# tube = []
-# for i in range(n):
-#     tube.append(list(map(int,sys.stdin.readline().split())))
-# s,x,y = map(int,sys.stdin.readline().split())
+n,k = map(int,sys.stdin.readline().split())
+tube = []
+for i in range(n):
+    tube.append(list(map(int,sys.stdin.readline().split())))
+s,x,y = map(int,sys.stdin.readline().split())
 
 # n,k = 3,3
 # tube = [[1, 0, 2], [0, 0, 0], [3, 0, 0]]
 # s,x,y = 2,3,2
 
-n,k = 3,3
-tube = [[1, 0, 2], [0, 0, 0], [3, 0, 0]]
-s,x,y = 1,2,2
+# n,k = 3,3
+# tube = [[1, 0, 2], [0, 0, 0], [3, 0, 0]]
+# s,x,y = 1,2,2
+
+# 바이러스 추출
+virus_dict = {}
+for i in range(n):
+    for j in range(n):
+        if tube[i][j] != 0:
+            key = tube[i][j]
+            val = [i,j]
+            virus_dict[key] = val
+
+# 추출된 바이러스 오름차순 정렬
+sorted_virus_dict = sorted(virus_dict.items())
+
+# a_dict = {2: [0, 2], 1: [0, 0], 3: [2, 0], 0: [9 ,1]}
+# sorted_dict = sorted(a_dict.items())
+# print(sorted_dict)
+# for a,[b,c] in sorted_dict:
+#     print(a,b,c)
 
 # 초기값 설정
 q = deque([])
-for virus in range(1,k+1):
-    for i in range(n):
-        for j in range(n):
-            if tube[i][j] == virus:
-                q.append([0,virus,i,j])
+for virus,[i,j] in sorted_virus_dict:
+    q.append([0,virus,i,j])
+
+# 초기값 설정
+# q = deque([])
+# for virus in range(1,k+1):
+#     for i in range(n):
+#         for j in range(n):
+#             if tube[i][j] == virus:
+#                 q.append([0,virus,i,j])
 
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
