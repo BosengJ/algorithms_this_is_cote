00799fc21c0f43ade78260f1312cf1bf74a4b5b7
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-09-08 11:47:16 +0900

    Update 32_연산자끼워넣기.py

--- 32_연산자끼워넣기.py
+++ 32_연산자끼워넣기.py
@@ -22,3 +22,25 @@ def findMin(li):
         if n < m:
             m = n
     return m
+
+def dfs(i, res, add, sub, mul, div):
+    global max_, min_
+    if i == n:
+        max_ = findMax(li) # li
+        min_ = findMin(li) # li
+        return
+    else:
+        if add:
+            dfs(i+1, res+num[i], add-1, sub, mul, div)
+        if sub:
+            dfs(i+1, res-num[i], add, sub-1, mul, div)
+        if mul:
+            dfs(i+1, res*num[i], add, sub, mul-1, div)
+        if div:
+            dfs(i+1, res//num[i], add, sub, mul, div-1)
+
+max_, min_ = 1e9, -1e9
+dfs(1,num[0],add,sub,mul,div)
+
+print(max_)
+print(min_)
