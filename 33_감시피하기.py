61969c7b8424ea1e2d88947743f18a2ba4ecdda2
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-09-09 11:18:44 +0900

    Create 33_감시피하기.py

---
+++ 33_감시피하기.py
@@ -0,0 +1,15 @@
+import sys
+
+# n = int(sys.stdin.readline())
+# maps = []
+# for i in range(n):
+#     maps.append(list(map(str,sys.stdin.readline().split())))
+
+n = 5
+maps = [
+    ['X', 'S', 'X', 'X', 'T'], 
+    ['T', 'X', 'S', 'X', 'X'], 
+    ['X', 'X', 'X', 'X', 'X'], 
+    ['X', 'T', 'X', 'X', 'X'], 
+    ['X', 'X', 'T', 'X', 'X']
+    ]
