ece7d476f97a237310148e1e1cdbd7fee478df86
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-09-15 17:37:34 +0900

    Update 33_감시피하기.py

--- 33_감시피하기.py
+++ 33_감시피하기.py
@@ -6,23 +6,23 @@ from collections import deque
 # for i in range(n):
 #     maps.append(list(map(str,sys.stdin.readline().split())))
 
-# n = 5
-# maps = [
-#     ['X', 'S', 'X', 'X', 'T'], 
-#     ['T', 'X', 'S', 'X', 'X'], 
-#     ['X', 'X', 'X', 'X', 'X'], 
-#     ['X', 'T', 'X', 'X', 'X'], 
-#     ['X', 'X', 'T', 'X', 'X']
-#     ]
-
 n = 5
 maps = [
-    ['X', 'S', 'X', 'O', 'T'], 
-    ['T', 'O', 'S', 'X', 'X'], 
-    ['X', 'X', 'O', 'X', 'X'], 
+    ['X', 'S', 'X', 'X', 'T'], 
+    ['T', 'X', 'S', 'X', 'X'], 
+    ['X', 'X', 'X', 'X', 'X'], 
     ['X', 'T', 'X', 'X', 'X'], 
     ['X', 'X', 'T', 'X', 'X']
     ]
+
+# n = 5
+# maps = [
+#     ['X', 'S', 'X', 'O', 'T'], 
+#     ['T', 'O', 'S', 'X', 'X'], 
+#     ['X', 'X', 'O', 'X', 'X'], 
+#     ['X', 'T', 'X', 'X', 'X'], 
+#     ['X', 'X', 'T', 'X', 'X']
+#     ]
 
 # BFS 진행시, 상하좌우로 감시하는 함수
 def up(x,y,li):
@@ -61,7 +61,6 @@ def right(x,y,li):
             break
     return li
 
-
 # 선생님이 감시하는 BFS 함수
 def teacherWatchBFS(li,n):
     q = deque([])
@@ -79,16 +78,39 @@ def teacherWatchBFS(li,n):
         
     return li
 
-new_maps = teacherWatchBFS(maps,n)
+# 지도를 확인해서 선생님의 감시를 피했는지 여부를 확인하는 함수
+def checkmaps(new_maps):
+    answer = ''
+    for i in range(len(new_maps)):
+        for j in range(len(new_maps)):
+            if "catch" not in new_maps[i][j]:
+                answer = "YES"
+    return answer
+
+# 3개의 장애물을 설치하는 모든 경우의 수
+def obstacleDFS(cnt_obs,start,maps,n):
+    if cnt_obs == 3:
+        new_maps = teacherWatchBFS(maps,n)
+        answer = checkmaps(new_maps)
+        if len(answer) == 0:
+            answer = "NO"
+        return
+    
+    for i in range(start, n*n):
+        x = (int)(i / n)
+        y = (int)(i % n)
+        if maps[x][y] == 'X':
+            maps[x][y] = 'O'
+            obstacleDFS(cnt_obs+1,i+1)
+
+            # obstacleDFS에서 빠져 나오면 원회복
+            maps[x][y] = 'X'
+
+
+new_maps = obstacleDFS()
 print(new_maps)
 
-answer = ''
-for i in range(len(new_maps)):
-    for j in range(len(new_maps)):
-        if "catch" in new_maps[i][j]:
-            answer = "NO"
-            break
-        
-if len(answer) == 0:
-    answer = "YES"
+
+
+
 print(answer)
