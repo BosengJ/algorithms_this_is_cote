3f289445baaae471915d047088f3abdf55c1d6b4
BosengJ <65713153+BosengJ@users.noreply.github.com>
2021-09-15 17:09:36 +0900

    Update 33_감시피하기.py

--- 33_감시피하기.py
+++ 33_감시피하기.py
@@ -6,23 +6,23 @@ from collections import deque
 # for i in range(n):
 #     maps.append(list(map(str,sys.stdin.readline().split())))
 
-n = 5
-maps = [
-    ['X', 'S', 'X', 'X', 'T'], 
-    ['T', 'X', 'S', 'X', 'X'], 
-    ['X', 'X', 'X', 'X', 'X'], 
-    ['X', 'T', 'X', 'X', 'X'], 
-    ['X', 'X', 'T', 'X', 'X']
-    ]
-
 # n = 5
 # maps = [
-#     ['X', 'S', 'X', 'O', 'T'], 
-#     ['T', 'O', 'S', 'X', 'X'], 
-#     ['X', 'X', 'O', 'X', 'X'], 
+#     ['X', 'S', 'X', 'X', 'T'], 
+#     ['T', 'X', 'S', 'X', 'X'], 
+#     ['X', 'X', 'X', 'X', 'X'], 
 #     ['X', 'T', 'X', 'X', 'X'], 
 #     ['X', 'X', 'T', 'X', 'X']
 #     ]
+
+n = 5
+maps = [
+    ['X', 'S', 'X', 'O', 'T'], 
+    ['T', 'O', 'S', 'X', 'X'], 
+    ['X', 'X', 'O', 'X', 'X'], 
+    ['X', 'T', 'X', 'X', 'X'], 
+    ['X', 'X', 'T', 'X', 'X']
+    ]
 
 # BFS 진행시, 상하좌우로 감시하는 함수
 def up(x,y,li):
@@ -82,9 +82,13 @@ def teacherWatchBFS(li,n):
 new_maps = teacherWatchBFS(maps,n)
 print(new_maps)
 
+answer = ''
 for i in range(len(new_maps)):
     for j in range(len(new_maps)):
-        if new_maps[i][j] == "catch":
+        if "catch" in new_maps[i][j]:
             answer = "NO"
             break
+        
+if len(answer) == 0:
+    answer = "YES"
 print(answer)
