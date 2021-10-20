80f4ea961b6dca62e88436eb23e96ac9741d1238
BosengJ <boseungee22@gmail.com>
2021-10-20 18:00:33 +0900

    final

--- 복습3/02_숫자카드게임.py
+++ 복습3/02_숫자카드게임.py
@@ -1,12 +1,12 @@
 # input
-# n,m = map(int,input("행의 개수 n과 열의 개수 m을 공백을 기준으로 하여 입력하세요 >").split())
-# cards = []
-# for i in range(n):
-#     cards.append(list(map(int,input("n개의 줄에 걸쳐 각 카드에 적힌 숫자를 공백으로 구분하여 입력하세요 >").split())))
+n,m = map(int,input("행의 개수 n과 열의 개수 m을 공백을 기준으로 하여 입력하세요 >").split())
+cards = []
+for i in range(n):
+    cards.append(list(map(int,input("n개의 줄에 걸쳐 각 카드에 적힌 숫자를 공백으로 구분하여 입력하세요 >").split())))
 
 # test
-n,m = 3,3
-cards = [[3, 1, 2], [4, 1, 4], [2, 2, 2]]
+# n,m = 3,3
+# cards = [[3, 1, 2], [4, 1, 4], [2, 2, 2]]
 
 # 가장 작은 숫자를 뽑아내는 함수
 def checkMin(li):
