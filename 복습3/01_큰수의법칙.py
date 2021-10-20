76199439eab512fa03528c68e7b76e099d8db130
BosengJ <boseungee22@gmail.com>
2021-10-20 14:45:44 +0900

    오름차순 정렬 함수 구현
    - 오름차순 정렬 함수 구현
    - test case 추가

--- 복습3/01_큰수의법칙.py
+++ 복습3/01_큰수의법칙.py
@@ -1,4 +1,15 @@
 # input
-n,m,k = map(int,input("n,m.k를 공백으로 구분하여 입력하세요 >").split())
-nums = list(map(int,input("공백으로 구분하여 숫자를 입력하세요 >").split()))
+# n,m,k = map(int,input("n,m.k를 공백으로 구분하여 입력하세요 >").split())
+# nums = list(map(int,input("공백으로 구분하여 숫자를 입력하세요 >").split()))
 
+# test case
+n,m,k = 5,8,3
+nums = [2,4,5,4,6]
+
+# 오름차순으로 정렬하는 함수
+def MinToMax(li):
+    for i in range(len(li)):
+        for j in range(len(li)-i-1):
+            if li[j] > li[j+1]:
+                li[j], li[j+1] = li[j+1], li[j]
+    return li
