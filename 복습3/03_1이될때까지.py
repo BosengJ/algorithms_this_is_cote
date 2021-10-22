507727392bb1bb568bf6394d7f426dcd3abe664c
BosengJ <boseungee22@gmail.com>
2021-10-22 12:56:10 +0900

    while문을 통해 n을 1로 만드는 최소 횟수 구현

--- 복습3/03_1이될때까지.py
+++ 복습3/03_1이될때까지.py
@@ -1,3 +1,12 @@
 # input
 n,k = map(int,input("n, k를 공백으로 구분하여 입력하세요 (단, n은 항상 k보다 크거나 같아야 합니다) >").split())
 
+cnt = 0
+while n > 1:
+    if n % k == 0:
+        n //= k
+    else:
+        n -= 1
+    cnt += 1
+
+print(cnt)
