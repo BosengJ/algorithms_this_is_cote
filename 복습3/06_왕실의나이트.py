0712784c73e08d5ca9bef733eefdc7e9579270d5
BosengJ <boseungee22@gmail.com>
2021-10-22 16:31:45 +0900

    currentLocation 함수 생성
    행렬을 수치화 해서 각각의 변수에 저장해주는 부분을 함수로 만들어줬다

--- 복습3/06_왕실의나이트.py
+++ 복습3/06_왕실의나이트.py
@@ -1,9 +1,12 @@
 # input
 location = input("현재 나이트가 위치한 곳의 좌표를 나타내는 두 문자로 구성된 문자열을 입력하세요 >")
 
-# 행렬을 수치화 해서 각각의 변수에 저장해준다
-x = int(location[1])
-y = int(ord(location[0])) - int(ord('a')) + 1
-print(x)
-print(y)
+# 행렬을 수치화 해서 각각의 변수에 저장해주는 함수
+def currentLocation(str):
+    row = int(str[1])
+    column = int(ord(str[0])) - int(ord('a')) + 1
+    return row, column
+
+x,y = currentLocation(location)
+print(x,y)
 
