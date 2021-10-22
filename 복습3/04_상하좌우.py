54d97c3d205e93fe0d14457d1e66c8aa2854d6c2
BosengJ <boseungee22@gmail.com>
2021-10-22 13:04:58 +0900

    Test case

--- 복습3/04_상하좌우.py
+++ 복습3/04_상하좌우.py
@@ -1,4 +1,7 @@
 # input
-n = int(input("공간의 크기를 나타내는 n을 입력하세요 >"))
-plan = list(input("여행가 A가 이동할 계획서 내용을 공백을 기준으로 입력하세요 >").split())
+# n = int(input("공간의 크기를 나타내는 n을 입력하세요 >"))
+# plan = list(input("여행가 A가 이동할 계획서 내용을 공백을 기준으로 입력하세요 >").split())
 
+# test case
+n = 5
+plan = ['R', 'R', 'R', 'U', 'D', 'D']
