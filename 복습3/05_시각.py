4accec216a991d7008a77262bb454ebd4e0a8659
BosengJ <boseungee22@gmail.com>
2021-10-22 15:32:10 +0900

    3이 있을 때 마다 카운트 업 하는 for 반복문 구현

--- 복습3/05_시각.py
+++ 복습3/05_시각.py
@@ -1,3 +1,13 @@
 # input
 n = int(input("정수 n을 입력하세요 >"))
 
+# 3이 있을 때 마다 cnt +1 한다
+cnt = 0
+for hour in range(n+1):
+    for min in range(60):
+        for sec in range(60):
+            if '3' in str(hour) + str(min) + str(sec):
+                cnt += 1
+
+print(cnt)
+
