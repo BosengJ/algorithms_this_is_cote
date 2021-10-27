33a4a868822058c9d1fae9b99afa42bf161d4f70
BosengJ <boseungee22@gmail.com>
2021-10-27 15:58:17 +0900

    input 살려줌

--- 복습3/07_게임개발.py
+++ 복습3/07_게임개발.py
@@ -29,11 +29,11 @@ import copy
 # 답 4
 
 # input
-# n,m = map(int,input("맵의 세로 크기 n과 가로 크기 m을 공백으로 구분하여 입력하세요").split())
-# x,y,d = map(int,input("게임 캐릭터가 있는 칸의 좌표 x,y와 바라보는 방향 d를 공백으로 구분하여 입력하세요 >").split())
-# maps = []
-# for i in range(n):
-#     maps.append(list(map(int,input("n개의 줄에 맵의 상태가 북쪽부터 남쪽 순서대로, 각 줄의 데이터는 서쪽부터 동쪽 순서대로 입력하세요 >").split())))
+n,m = map(int,input("맵의 세로 크기 n과 가로 크기 m을 공백으로 구분하여 입력하세요").split())
+x,y,d = map(int,input("게임 캐릭터가 있는 칸의 좌표 x,y와 바라보는 방향 d를 공백으로 구분하여 입력하세요 >").split())
+maps = []
+for i in range(n):
+    maps.append(list(map(int,input("n개의 줄에 맵의 상태가 북쪽부터 남쪽 순서대로, 각 줄의 데이터는 서쪽부터 동쪽 순서대로 입력하세요 >").split())))
 
 # 뒤로 물러나는 함수
 def backStep(li,x,y):
