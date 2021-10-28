b4ea97959584fe6a66b2592f244e14a067d6061e
BosengJ <boseungee22@gmail.com>
2021-10-28 15:11:11 +0900

    checkZero 함수 구현 (bfs사용)
    error 뜬다!

--- 복습3/16_음료수얼려먹기.py
+++ 복습3/16_음료수얼려먹기.py
@@ -12,6 +12,18 @@ mold = [[0, 0, 1, 1, 0], [0, 0, 0, 1, 1], [1, 1, 1, 1, 1], [0, 0, 0, 0, 0]]
 
 # bfs 방법으로 구멍이 뚫려 있는 부분(0)을 확인한다
 def checkZero(p,q,li):
+    queue = deque([p,q])    # 초기값 넣어준다
+    li[p][q] = 1            # 방문 처리를 1로 해준다
+    while queue:
+        x,y = queue.popleft()
+        fourDirections = [[-1,0], [0,1], [1,0], [0,-1]]  # north, east, south, west
+        for direction in fourDirections:
+            nx,ny = 0,0
+            nx = x + direction[0]
+            ny = y + direction[1]
+            if (0 <= nx) and (nx <= len(li[0])) and (0 <= ny) and (ny <= len(li)) and li[nx][ny] == 0:
+                queue.append([nx,ny])
+                li[nx][ny] = 1
     return li
 
 # for문을 사용하여 얼음 틀의 구멍이 뚫려 있는 부분(0)을 확인해 주고 bfs를 통해 생성되는 아이스크림 개수를 구한다
